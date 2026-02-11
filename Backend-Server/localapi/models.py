d7f715f06819d34c8956ed56e3dec3ebb9f81fc5
kromka45 <modern1995.hz@gmail.com>
2026-02-11 18:24:49 +0000

    adding better middleware cors and changing python ersion to 3.12

--- Backend-Server/localapi/models.py
+++ Backend-Server/localapi/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class LocalPlayer(models.Model):
     killer_bi_id = models.CharField(max_length=64, unique=True, db_index=True)
 
+    nick = models.CharField(max_length=64, blank=True, null=True)
+
     # cache selected fields from BIG DB (you can replace/expand these)
     big_payload = models.JSONField(default=dict, blank=True)
 
